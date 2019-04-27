eaa67dabcc397d5a219cdbdf034b102f9a61e7ba
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-27 18:49:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -116,5 +116,18 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  matches = []
+holiday_hash.each |season, holiday| do
+  holiday.each do |holiday, supplies|
+    if supplies.include? "BBQ"
+      matches << holiday
+    end
+    
+  end
+  
+end
+matches
+end
+  
 
 end
