b985f2e025eab989936562c5406dfaa042489a87
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-27 18:36:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -100,7 +100,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday_name, supplies|
-      holiday_capitalized = holiday_name.to_s.split(" ")
+      holiday_capitalized = holiday_name.to_s.split("_")
       puts holiday_capitalized
     end
 
